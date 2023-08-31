c8509bf52b0041792489a08c885e3e5bcbfdf53e
cindyjeby <cindyjeby@gmail.com>
2023-08-31 13:05:05 +0300

    is trr full

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance(const binary_tree_t *tree)
+ * binary_tree_balance - function that measures the balance factor of a binary tree
  * @tree: pointer to the root
  * Return: 0 if NULL
 */
