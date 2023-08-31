cc913732a637d2dd62470e580ec39eb10b761b3f
cindyjeby <cindyjeby@gmail.com>
2023-08-31 11:01:02 +0300

    in-order

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -5,7 +5,7 @@
  * @func: pointer to a function to call for each node
  * Return: Nothing
 */
-void binary_tree_inorder(const binary_tree_t *tree, void(*func))
+void binary_tree_inorder(const binary_tree_t *tree, void(*func)(int))
 {
     if (tree && func)
     {
