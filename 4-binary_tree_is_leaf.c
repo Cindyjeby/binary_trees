24ebecde96ed932af515508f5363e530459fccce
cindyjeby <cindyjeby@gmail.com>
2023-08-31 10:36:15 +0300

    checks if a node is aleaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,6 +8,7 @@ intbinary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
-	if (node->left == NULL && node->right == NULL)
+	if (node->left != NULL || node->right != NULL)
 		return (0);
+    return (1);
 }
