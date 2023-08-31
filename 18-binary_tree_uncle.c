ea46ec2c0bc54c32fa11ab9ef7e969ec5819d76a
cindyjeby <cindyjeby@gmail.com>
2023-08-31 16:53:21 +0300

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,5 +10,5 @@ binary_tree_t*binary_tree_uncle(binary_tree_t *node)
 			return (NULL);
 	if (node->parent == node->parent->parent->right)
 		return (node->parent->parent->left);
-	return (node->parent->parent-right);
+	return (node->parent->parent->right);
 }
