fda148336cf40fc7a35c8c509d81e1ef527e205d
cindyjeby <cindyjeby@gmail.com>
2023-08-31 16:31:49 +0300

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,7 +8,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->left == NULL && node->right == NULL)
+	else if (node->left == NULL && node->right == NULL)
 		return (1);
 	return (0);
 }
@@ -25,7 +25,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	lefth = binary_tree_height(tree->left);
 	righth = binary_tree_height(tree->right);
 
-	if (righth < = lefth)
+	if (righth <= lefth)
 		return (lefth + 1);
 
 	return (righth + 1);
@@ -56,8 +56,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	subleft = binary_tree_height(left);
 
 	if (subright == subleft)
-
-	{
+    {
 		if (binary_tree_is_perfect(left) && binary_tree_is_perfect(right))
 			return (1);
 	}
