54368801d31826e8a7c3f7247621c27ab435a0a2
cindyjeby <cindyjeby@gmail.com>
2023-08-31 16:56:25 +0300

    uncle is pissing me off at this point

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -4,7 +4,7 @@
  * @node: pointer to the node
  * Return: pointer to the uncle node or NULL
 */
-binary_tree_t*binary_tree_uncle(binary_tree_t *node)
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
